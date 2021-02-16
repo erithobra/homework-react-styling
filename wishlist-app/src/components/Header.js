7b36fb18862f85bbe35d3f75ae86b7b2aee55cea
ericb2020 <ebrauer83@gmail.com>
2021-02-16 17:37:35 -0600

    header styling

--- wishlist-app/src/components/Header.js
+++ wishlist-app/src/components/Header.js
@@ -2,10 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header() {
+    const Header = styled.header`
+        background-color: #F9F854;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-left: 10px;
+    `
+
+    const H1 = styled.h1`
+        width: 50%;
+    `
+
+    const Nav = styled.nav`
+        display: flex;
+        justify-content: flex-end;
+        width: 50%;
+
+
+    `
+    const A = styled.a`
+        margin: 0px 10px 0px 10px;
+
+    `
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+        <Header>
+            <H1>Wishlist App</H1>
+            <Nav>
+                <A>Blog</A>
+                <A>Lists</A>
+                <A>Sign Up</A>
+                
+            </Nav>
+        </Header>
     )
 }
 
