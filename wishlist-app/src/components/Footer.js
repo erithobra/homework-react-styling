f46fe9b6e012147c0bdcdd1f41e0dec4bb73e184
ericb2020 <ebrauer83@gmail.com>
2021-02-16 17:54:43 -0600

    footer styling and some list styling

--- wishlist-app/src/components/Footer.js
+++ wishlist-app/src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+
 function Footer() {
+    const Footer = styled.footer`
+    background-color: #F9F854;
+    height: 30px;
+`
     return (
-        <footer>
-            <h1>Footer</h1>
-        </footer>
+        <Footer></Footer>
     )
 }
 
--- wishlist-app/src/components/List.js
+++ wishlist-app/src/components/List.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+
 function List() {
+    const List = styled.div`
+        display: flex;
+        justify-content: center;
+    `
+
+    const H1 = styled.h1`
+    width: 75%;
+    background-color: #EAB444;
+    display: flex;
+    justify-content: center;
+    height: 75px;
+    align-items: center; 
+    `
+
     return (
-        <div>
-            <h1>List</h1>
-        </div>
+        <List>
+            <H1>Your List</H1>
+        </List>
     )
 }
 
